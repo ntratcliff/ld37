f590ffe7faf4845bf7c98124a038aa3b9fa18b05
Noah Ratcliff <me@nratcliff.com>
2016-12-10 15:49:52 -0500

    scoring

---
+++ Assets/Scripts/Scorable.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Scorable : MonoBehaviour 
+{
+    public float Value = 1f;
+
+    private float sum;
+    private float maxDeltaScore;
+
+    public int Score
+    {
+        get { return Mathf.RoundToInt(sum); }
+    }
+
+    // scoring variables
+    private Vector3 rootPos;
+    private Vector3 rootAngles;
+    private float mass;
+
+	// Use this for initialization
+	void Start () 
+	{
+        rootPos = transform.position;
+        rootAngles = transform.eulerAngles;
+        mass = GetComponent<Rigidbody>().mass;
+	}
+	
+    private void scoreObject()
+    {
+        // get delta orientation
+        Vector3 deltaPos = transform.position - rootPos;
+        Vector3 deltaAngle = transform.eulerAngles - rootAngles;
+
+        // convert angles from degrees to radians before getting scalar
+        deltaAngle *= Mathf.Deg2Rad;
+
+        // calculate delta orienation score
+        float deltaScore = (deltaPos.magnitude) * mass * Value;
+        if(deltaScore > maxDeltaScore)
+            maxDeltaScore = deltaScore;
+
+        // set sum as max delta orientation score (TODO: velocity and impact scoring?)
+        sum = maxDeltaScore;
+    }
+
+	// Update is called once per frame
+	void Update () 
+	{
+        scoreObject();
+	}
+}
---
+++ Assets/Scripts/Scoreboard.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class Scoreboard : MonoBehaviour 
+{
+    public int Score;   
+
+    private Text scoreText;
+
+    // scorable objects in scene
+    private Scorable[] scorables;
+
+	// Use this for initialization
+	void Start () 
+	{
+        scoreText = GetComponent<Text>();
+
+        scorables = FindObjectsOfType<Scorable>();
+	}
+	
+    private void updateGlobalScore()
+    {
+        int sum = 0;
+        for (int i = 0; i < scorables.Length; i++)
+        {
+            sum += scorables[i].Score;
+        }
+
+        //if (sum > Score)
+            Score = sum;
+    }
+
+    public void updateScoreText()
+    {
+        scoreText.text = "Score: <b>" + Score + "</b>";
+    }
+
+	// Update is called once per frame
+	void Update () 
+	{
+        updateGlobalScore();
+        updateScoreText();
+	}
+}
