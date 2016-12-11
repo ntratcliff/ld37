deb80109b4d66d180abd713845e86559f1870f1a
Noah Ratcliff <me@nratcliff.com>
2016-12-10 20:03:00 -0500

    change scene after round end

---
+++ Assets/Scripts/ChangeSceneOnEnter.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ChangeSceneOnEnter : MonoBehaviour
+{
+
+    public string Scene;
+    public float Delay;
+
+    private Coroutine coroutine;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        coroutine = StartCoroutine(ChangeSceneDelay());
+    }
+
+    IEnumerator ChangeSceneDelay()
+    {
+        yield return new WaitForSeconds(Delay);
+        SceneManager.LoadScene(Scene);
+    }
+}
