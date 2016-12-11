c37852880e5eda265dffcea5553fd09afe2a6e46
Noah Ratcliff <me@nratcliff.com>
2016-12-10 19:08:11 -0500

    fade out in hallway after round

---
+++ Assets/Scripts/ColorFromDist.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ColorFromDist : MonoBehaviour 
+{
+    public Transform A, B;
+
+    public Color MinColor, MaxColor;
+    public float MinDist, MaxDist;
+
+    Image uiImage;
+
+	// Use this for initialization
+	void Start () 
+	{
+        uiImage = GetComponent<Image>();	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+        // get distance between A and B
+        float dist = Vector3.Distance(A.position, B.position);
+
+        // get scalar from dist
+        float t = (dist - MinDist) / (MaxDist - MinDist);
+
+        // lerp color by scalar
+        uiImage.color = Color.Lerp(MinColor, MaxColor, t);
+	}
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,11 +9,20 @@ public class GameManager : MonoBehaviour
 
     private IRoundStatusTarget roundStatTargets;
 
+    public GameObject[] EnableOnStart;
+    public GameObject[] EnableOnEnd;
+
 	// Use this for initialization
 	void Start () 
 	{
         timer = FindObjectOfType<RoundTime>();
         scoreboard = FindObjectOfType<Scoreboard>();
+
+        // enable start objects
+        for (int i = 0; i < EnableOnStart.Length; i++)
+        {
+            EnableOnStart[i].SetActive(true);
+        }
 
         // start the round
         executeStatusRecursive(this.gameObject, null, (x, y) => x.RoundStart());
@@ -21,6 +30,13 @@ public class GameManager : MonoBehaviour
 
     public void GameOver()
     {
+        // enable end objects
+        for (int i = 0; i < EnableOnEnd.Length; i++)
+        {
+            EnableOnEnd[i].SetActive(true);
+        }
+
+        // call delegates
         executeStatusRecursive(this.gameObject, null, (x, y) => x.RoundEnd());
     }
 
