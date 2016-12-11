ddee7212d88d979fcd1318ab1b7c651700c0feea
Noah Ratcliff <me@nratcliff.com>
2016-12-11 15:14:19 -0500

    screenshot system

---
+++ Assets/Scripts/CopyText.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CopyText : MonoBehaviour 
+{
+    public Text Copy;
+	// Update is called once per frame
+	void Update () 
+	{
+        GetComponent<Text>().text = Copy.text;
+	}
+}
---
+++ Assets/Scripts/Screenshot.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Screenshot : MonoBehaviour
+{
+    public KeyCode Key;
+    public GameObject[] DisableForShot;
+    public GameObject[] EnableForShot;
+    public string Prefix = "Screenshot_";
+
+
+    private void setShotObjectsState(GameObject[] arr ,bool state)
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            arr[i].SetActive(state);
+        }
+    }
+
+    private IEnumerator captureScreen()
+    {
+        setShotObjectsState(DisableForShot, false);
+        setShotObjectsState(EnableForShot, true);
+
+        yield return new WaitForEndOfFrame();
+
+        // take screenshot
+        string filename = Prefix + DateTime.Now.ToString("yyyy-MM-dd HH-mm-ss") + ".png";
+        Application.CaptureScreenshot(filename);
+        Debug.Log(filename);
+
+        setShotObjectsState(DisableForShot, true);
+        setShotObjectsState(EnableForShot, false);
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(Key))
+            StartCoroutine(captureScreen());
+    }
+}
