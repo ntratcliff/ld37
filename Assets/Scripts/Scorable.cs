432ad052bbf7dbca9b87ce594fbdb807aead3683
Noah Ratcliff <me@nratcliff.com>
2016-12-10 19:42:57 -0500

    disable collisions on round end

--- Assets/Scripts/Scorable.cs
+++ Assets/Scripts/Scorable.cs
@@ -59,9 +59,49 @@ public class Scorable : MonoBehaviour, IRoundStatusTarget
         roundActive = true;
     }
 
+    private Color setSaturation(Color c, float sat)
+    {
+        float h, s, v;
+        Color.RGBToHSV(c, out h, out s, out v);
+        s = sat;
+        return Color.HSVToRGB(h, s, v);
+    }
+
+    private void desaturateMaterial()
+    {
+        MeshRenderer meshRenderer = GetComponent<MeshRenderer>();
+        if (meshRenderer)
+        {
+            foreach (Material mat in meshRenderer.materials)
+            {
+                mat.color = setSaturation(mat.color, 0.05f);
+
+                if (mat.GetColor("_EmissionColor") != null)
+                    mat.SetColor("_EmissionColor", setSaturation(mat.GetColor("_EmissionColor"), 0.05f));
+            }
+        }
+    }
+
+    private void disableColliders()
+    {
+        Collider[] attached = GetComponents<Collider>();
+        for (int i = 0; i < attached.Length; i++)
+        {
+            attached[i].enabled = false;
+        }
+
+        Collider[] children = GetComponentsInChildren<Collider>();
+        for (int i = 0; i < children.Length; i++)
+        {
+            children[i].enabled = false;
+        }
+    }
+
     public void RoundEnd()
     {
         roundActive = false;
         GetComponent<Rigidbody>().isKinematic = true;
+        disableColliders();
+        desaturateMaterial();
     }
 }
