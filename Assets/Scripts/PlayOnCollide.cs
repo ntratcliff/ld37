6a2391c1dcf3c779ae2f0fd4d0adf42499637309
Noah Ratcliff <me@nratcliff.com>
2016-12-11 19:42:31 -0500

    foley

---
+++ Assets/Scripts/PlayOnCollide.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class PlayOnCollide : MonoBehaviour
+{
+    private AudioSource source;
+    public float MinImpact = 1f;
+
+    public float MinPitch = 0.5f, MaxPitch = 1f;
+
+    // Use this for initialization
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (!source.isPlaying && collision.relativeVelocity.sqrMagnitude > Mathf.Pow(MinImpact, 2))
+        {
+            source.pitch = Random.Range(MinPitch, MaxPitch);
+            source.Play();
+        }
+    }
+}
